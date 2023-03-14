9886a2d7f86491c192ad5fedefc9e05f0884ad54
David Villeda <42588903+DavidITT@users.noreply.github.com>
2023-03-14 11:09:53 -0600

    useFetch

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,8 +1,36 @@
+import {useFetch} from "../hooks/useFetch.js";
+import {useState} from "react";
+import {useCounter} from "../hooks/useCounter.js";
+
 export const MultipleCustomHooks = () => {
+
+    const { counter, increment } = useCounter(1);
+    const {data, isLoading, hasError} = useFetch( `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`)
+    const {author, quote} = !!data && data[0]; //if is true execute...
+
     return (
         <>
             <h1>BreakingBad Quotes</h1>
             <hr/>
+
+            {
+                isLoading
+                    ?
+                    <div className="d-flex flex-row justify-content-center align-items-center">
+                        <div className="spinner-border text-primary" role="status"></div>
+                        <span className="text-primary">&nbsp; Cargando...</span>
+                    </div>
+                    :
+                    <blockquote className="blockquote text-end">
+                        <p className="mb-3">{quote}</p>
+                        <footer className="blockquote-footer">{author}</footer>
+                    </blockquote>
+            }
+
+            <button className="btn btn-primary" onClick={ () => increment()} disabled={isLoading}>
+                Siguiente
+            </button>
+
         </>
     )
 }
